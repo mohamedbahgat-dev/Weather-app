45ba84868041a7292e33bc4fdcb0e4748b6a8647
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-05-26 13:21:04 +0300

    building and styling navigation bar component

--- weather-app/src/components/Navigation/NavBar.tsx
+++ weather-app/src/components/Navigation/NavBar.tsx
@@ -1,9 +1,38 @@
 import "./NavBar.css";
+import { IoLocationOutline } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
+import { IoMdNotifications } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
 
 const NavBar = () => {
   return (
     <div>
-      <h2 className="header">This is a nav Bar</h2>
+      <nav className="nav">
+        <div className="logo-container">
+          <img
+            className="logo-img"
+            src="../public/logo.png"
+            alt="SkyNow logo"
+          />
+        </div>
+        <div className="location-info">
+          <IoLocationOutline size="1.5rem" />
+          <h3>Cairo, Egypt</h3>
+        </div>
+        <form className="search-form">
+          <input type="text" placeholder="search for a location" />
+          <button className="search-btn" type="submit">
+            <IoSearchOutline size="15px" />
+          </button>
+        </form>
+        <div className="notification-icon">
+          <IoMdNotifications color="#332D56" size="1.5rem" />
+        </div>
+        <div className="user-info">
+          <FaUser color="gray" size="1.5rem" />
+          <h3>Tayseer Mohamed</h3>
+        </div>
+      </nav>
     </div>
   );
 };

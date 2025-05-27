73e231a28858c1184a7e534e3275e0111faf2acb
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-05-27 14:09:49 +0300

    creating static MainDashboard component

--- weather-app/src/components/MainDashboard/MainDashboard.tsx
+++ weather-app/src/components/MainDashboard/MainDashboard.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
+import { WiCelsius } from "react-icons/wi";
 import "./MainDashboard.css";
+import sun from "./../../../public/sun.png";
 
-const MainDashboard = () => {
+const MainDashboard: React.FC = () => {
+  const [datetime, setDatetime] = useState<Date>(new Date());
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setDatetime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timeInterval);
+  });
+
   return (
     <section className="dashboard">
       <div className="header">
         <p>Current Weather</p>
-        <h3>Time Now</h3>
+        <p>{datetime.toLocaleTimeString()}</p>
       </div>
       <div className="temp-card">
-        <img src="#" alt="weather icon" />
-        <h2>Temp</h2>
+        <img src={sun} alt="weather icon" />
+        <div className="temp">
+          <h1>30</h1>
+          <WiCelsius size={30} />
+        </div>
         <p>Weather note</p>
       </div>
       <article>Note about weather condition</article>

38a5fd72958916b13052973f4e92f470cd4483a6
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-05-27 05:54:58 +0300

    start building mainDashboard component

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/Navigation/NavBar";
+import MainDashboard from "./components/MainDashboard/MainDashboard";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <MainDashboard />
     </div>
   );
 };
--- weather-app/src/components/MainDashboard/MainDashboard.tsx
+++ weather-app/src/components/MainDashboard/MainDashboard.tsx
@@ -2,9 +2,18 @@ import "./MainDashboard.css";
 
 const MainDashboard = () => {
   return (
-    <div>
-      <h2>Main Dashboard</h2>
-    </div>
+    <section className="dashboard">
+      <div className="header">
+        <p>Current Weather</p>
+        <h3>Time Now</h3>
+      </div>
+      <div className="temp-card">
+        <img src="#" alt="weather icon" />
+        <h2>Temp</h2>
+        <p>Weather note</p>
+      </div>
+      <article>Note about weather condition</article>
+    </section>
   );
 };
 

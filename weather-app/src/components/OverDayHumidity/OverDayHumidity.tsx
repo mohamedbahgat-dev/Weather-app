230868189b8c9c6d21369f90028e66026480418e
mohamedbahgat <mohamedbahgat950@gmail.com>
2025-07-24 20:31:40 +0300

    remove overdayHumidity component

--- weather-app/src/components/OverDayHumidity/OverDayHumidity.tsx
+++
@@ -1,9 +0,0 @@
-import "./OverDayHumidity.css";
-import { useForcastData } from "../../Store";
-
-const OverDayHumidity = () => {
-  const { forcastedData } = useForcastData();
-  return <div>OverDayHumidity</div>;
-};
-
-export default OverDayHumidity;
